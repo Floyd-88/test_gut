774d3463d643b4cf097a06eb3f88e3b266e81bee
Dio Eracler <dioeracler1988@gmail.com>
2024-12-22 00:20:53 +0300

    text about

--- components/app/about/about.vue
+++ components/app/about/about.vue
@@ -22,7 +22,16 @@ const handlerClickLink = (block) => {
 </script>
 
 <template>
- <div>About</div>
+  <div>About
+  </div>
+  <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda atque corporis dicta ducimus fugiat, incidunt
+    ipsum laudantium magnam natus necessitatibus nesciunt officia officiis pariatur, possimus praesentium, sit tenetur
+    unde vero voluptas voluptatum. Ab ad, aliquam animi at atque consequatur consequuntur culpa deserunt dignissimos
+    doloremque error eum expedita fugiat fugit illo impedit inventore ipsam iste laboriosam, maiores minima neque nobis
+    odit perferendis quae, quibusdam quis quo ratione rem reprehenderit sapiente similique soluta sunt tempore unde
+    voluptates voluptatum? Aliquid amet, architecto atque aut commodi cupiditate dolorem, dolores, ea ex excepturi
+    explicabo iusto neque nulla quae quod recusandae similique sit soluta tenetur ut.
+  </div>
 </template>
 
 <style lang="less" src="./about.less" />
